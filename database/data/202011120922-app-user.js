ac6cabc27394f01ca8404d8b1d24a3926b39659e
zhanchao.wu <zhanchao.wu@icloud.com>
2020-11-20 16:46:05 +0800

    fix(user phone): user phone

--- database/data/202011120922-app-user.js
+++ database/data/202011120922-app-user.js
@@ -4,7 +4,7 @@ module.exports = [
     user_name: 'superadmin',
     real_name: '超级管理员',
     nick_name: '🦸‍♂️',
-    phone: '18554870324',
+    phone: '18899889988',
     app_user_type: 'system',
     password: 'e10adc3949ba59abbe56e057f20f883e', //123456
   },
