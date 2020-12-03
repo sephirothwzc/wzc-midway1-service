fd3cae5128e2bc852d8ef84728e64d8490e842ff
zhanchao.wu <zhanchao.wu@icloud.com>
2020-12-03 20:59:48 +0800

    feat(seed: app-user): seed:app-user

---
+++ database/data/202012032022-app-user-v1.2.js
@@ -0,0 +1,11 @@
+module.exports = [
+  {
+    user_gender: 'm',
+    user_name: 'xuncha',
+    real_name: '智能巡查',
+    nick_name: '🦸‍♂️',
+    phone: '13288888888',
+    app_user_type: 'system',
+    password: 'e10adc3949ba59abbe56e057f20f883e', //123456
+  },
+];
