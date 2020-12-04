57c0d1999420b37d15cc0b054752a11f5ab58fe7
zhanchao.wu <zhanchao.wu@icloud.com>
2020-12-04 14:57:49 +0800

    feat(test): test

--- test/app/controller/home.test.ts
+++ test/app/controller/home.test.ts
@@ -1,4 +1,5 @@
 const { app, assert } = require('midway-mock/bootstrap');
+import * as crypto from 'crypto';
 
 describe('test/app/controller/home.test.ts', () => {
   it('should assert', async () => {
@@ -14,5 +15,13 @@ describe('test/app/controller/home.test.ts', () => {
       .get('/api/hello')
       .expect('Welcome to new word!')
       .expect(200);
+  });
+  it('should GET /login', () => {
+    const pwd = 'gaosong123456';
+    const hash = crypto.createHash('md5');
+    hash.update(pwd);
+    const newpwd = hash.digest('hex');
+    console.log(newpwd);
+    assert(newpwd === 'f94057c1f1b4e05b53c15b801b0153a5');
   });
 });
