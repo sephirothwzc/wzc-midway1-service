a2e3fb87942adcebec6cb8c239c52171e42922c0
zhanchao.wu <zhanchao.wu@icloud.com>
2020-12-10 21:06:06 +0800

    feat(auth): auth

--- src/lib/hooks/app-user.hook.ts
+++ src/lib/hooks/app-user.hook.ts
@@ -1,6 +1,6 @@
 import * as _ from 'lodash';
 import { provide } from 'midway';
-import { Transaction } from 'sequelize/types';
+import { Op, Transaction } from 'sequelize';
 import { AppUserModel } from '../models/app-user.model';
 
 @provide('AppUserHook')
@@ -14,6 +14,30 @@ export class AppUserHook {
     }
     const result = await AppUserModel.findOne({
       where: { phone: appUserModel.phone },
+      transaction: options.transaction,
+    });
+    if (result) {
+      throw new Error('手机号已经存在');
+    }
+  }
+  async beforeUpdate(
+    appUserModel: AppUserModel,
+    options: { transaction: Transaction; validate: Boolean; returning: Boolean }
+  ) {
+    const changed = appUserModel.changed();
+    if (!changed) {
+      return;
+    }
+    if (!changed.includes('phone')) {
+      return;
+    }
+    const result = await AppUserModel.findOne({
+      where: {
+        phone: appUserModel.get('phone'),
+        id: {
+          [Op.ne]: appUserModel.get('id'),
+        },
+      },
       transaction: options.transaction,
     });
     if (result) {
