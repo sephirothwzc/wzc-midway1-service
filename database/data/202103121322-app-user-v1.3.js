7ab867306d95a5cc4ea0a1589844c4253e1bb482
王肇峰 <spiritf@foxmail.com>
2021-03-12 10:48:44 +0800

    feat: sequelize 数据库操作 增加 石景山\长辛店区分

---
+++ database/data/202103121322-app-user-v1.3.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    user_gender: 'm',
+    user_name: 'dafeng',
+    real_name: '王肇峰',
+    nick_name: '大峰',
+    phone: '13255580772',
+    app_user_type: 'system',
+  },
+  {
+    user_gender: 'm',
+    user_name: 'sunhao',
+    real_name: '孙颢',
+    nick_name: '孙颢',
+    phone: '18830728841',
+    app_user_type: 'system',
+  },
+  {
+    user_gender: 'm',
+    user_name: 'wangjingxin',
+    real_name: '王晶鑫',
+    nick_name: '晶鑫',
+    phone: '18811665003',
+    app_user_type: 'system',
+  },
+];
