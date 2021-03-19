ba8234d06016ada00cfa6a2fefc20986b64f9c23
zhanchao.wu <zhanchao.wu@icloud.com>
2021-03-19 14:24:21 +0800

    feat(union): union
    union

--- src/app/graphql/app-user-role/resolver.js
+++ src/app/graphql/app-user-role/resolver.js
@@ -7,8 +7,21 @@ module.exports = {
   Mutation,
   AppUserRole: {
     appUserIdObj: async (_root, _args, ctx, _info) => {
-      const service = await getService(ctx,'appUser');
+      const service = await getService(ctx, 'appUser');
       return service.fetchById(_root.appUserId);
+    },
+    typeIdModel: async (_root, _args, ctx, _info) => {
+      if (!_root.roleType || _root.roleType === 'none') {
+        return {};
+      }
+      const service = await getService(ctx, _root.roleType);
+      return service.fetchById(_root.typeId);
+    },
+  },
+  TypeIdModel: {
+    __resolveType: (obj, context, info) => {
+      const name = _.get(obj, '_modelOptions.name.plural', '');
+      return name.replace('Models', '');
     },
   },
 };
--- src/app/graphql/app-user/resolver.js
+++ src/app/graphql/app-user/resolver.js
@@ -6,6 +6,10 @@ module.exports = {
   Query,
   Mutation,
   AppUser: {
-
+    appUserRoleAppUserId: async (_root, _args, ctx, _info) => {
+      const service = await getService(ctx, 'appUserRole');
+      _.set(_args, 'param.where.appUserId', _root.id);
+      return service.findAll(_args.param);
+    },
   },
 };
