0029e5f351abc57c047449beea65da62ae910856
王肇峰 <spiritf@foxmail.com>
2021-03-26 15:10:57 +0800

    feat: 更新 stage 环境 redis密码

--- src/config/config.changxindian.stage.ts
+++ src/config/config.changxindian.stage.ts
@@ -80,7 +80,7 @@ export const redis = {
   client: {
     port: 6379,
     host: '127.0.0.1',
-    password: 'stage1234561qaz',
+    password: 'f761f589-A994-64ac-f93c@7b129c2b16b4',
     db: 1,
   },
 };
