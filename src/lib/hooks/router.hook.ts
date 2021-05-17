7656c48c5cd667aa301ea5b05be0d1b218ccd446
zhanchao.wu <zhanchao.wu@icloud.com>
2021-05-17 10:16:05 +0800

    feat(hook): hook
    hook

--- src/lib/hooks/router.hook.ts
+++ src/lib/hooks/router.hook.ts
@@ -4,31 +4,27 @@ import { Transaction } from 'sequelize/types';
 import { ROUTER, RouterModel } from '../models/router.model';
 import { ROUTER_ROLE, RouterRoleModel } from '../models/router-role.model';
 import * as Bb from 'bluebird';
-import { RouterModel } from '../models/router.model';
 
 @provide('RouterHook')
 export class RouterHook {
-
   async beforeDestroy(
     model: RouterModel,
     options: { transaction: Transaction; validate: Boolean; returning: Boolean }
   ) {
     const { routerIbfk1, routerRoleIbfk2 } = await Bb.props({
-        routerIbfk1: RouterModel.findOne({
-          where: {
-            [ROUTER.PARENT_ID]: model.get('id'),
-          },
-        }),
-        routerRoleIbfk2: RouterRoleModel.findOne({
-          where: {
-            [ROUTER_ROLE.ROUTER_ID]: model.get('id'),
-          },
-        }),
+      routerIbfk1: RouterModel.findOne({
+        where: {
+          [ROUTER.PARENT_ID]: model.get('id'),
+        },
+      }),
+      routerRoleIbfk2: RouterRoleModel.findOne({
+        where: {
+          [ROUTER_ROLE.ROUTER_ID]: model.get('id'),
+        },
+      }),
     });
     if (routerIbfk1 || routerRoleIbfk2) {
       throw new Error('已使用数据禁止删除');
     }
   }
-
-
 }
